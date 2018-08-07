651bcae6424b5f3c6e979dd83020a50d5d089cdb
chrisregan <32357325+chrisregan@users.noreply.github.com>
2018-08-07 16:05:23 +1200

    New commit by chris

---
+++ newfile.py
@@ -0,0 +1,10 @@
+ Volume in drive C is Windows
+ Volume Serial Number is 6448-61B1
+
+ Directory of C:\Users\CPRegan\CodeProjects
+
+07/08/2018  04:03 p.m.    <DIR>          .
+07/08/2018  04:03 p.m.    <DIR>          ..
+07/08/2018  04:03 p.m.                 0 newfile.py
+               1 File(s)              0 bytes
+               2 Dir(s)  453,814,571,008 bytes free
